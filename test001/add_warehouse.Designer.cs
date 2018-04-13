19ccfc6c28214b3ad88deb0dcf62e044ba5c0c37
kaushik ponugoti <kaushik@netreca.com>
2018-04-13 12:23:38 -0700

    update warehouse

---
+++ test001/add_warehouse.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace test001
+{
+    partial class add_warehouse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.btn_add_warehouse_save = new System.Windows.Forms.Button();
+            this.btn_add_warehouse_cancel = new System.Windows.Forms.Button();
+            this.txb_wh_id = new System.Windows.Forms.TextBox();
+            this.txb_wh_name = new System.Windows.Forms.TextBox();
+            this.txb_city = new System.Windows.Forms.TextBox();
+            this.txb_street = new System.Windows.Forms.TextBox();
+            this.txb_zip = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.txb_tax = new System.Windows.Forms.TextBox();
+            this.txb_country = new System.Windows.Forms.TextBox();
+            this.txb_mobile = new System.Windows.Forms.TextBox();
+            this.cbx_state = new System.Windows.Forms.ComboBox();
+            this.cbx_market = new System.Windows.Forms.ComboBox();
+            this.rbtn_active = new System.Windows.Forms.RadioButton();
+            this.rbtn_inactive = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Warehouse ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(130, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(102, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Market ID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(119, 216);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Street1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(146, 266);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "City";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(108, 312);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Zip Code";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(448, 68);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(108, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Submaster ID";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(483, 116);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Tax Rate";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(508, 165);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(48, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "State";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(492, 216);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(64, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Country";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(501, 266);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(55, 20);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Mobile";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(500, 312);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(56, 20);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Status";
+            // 
+            // btn_add_warehouse_save
+            // 
+            this.btn_add_warehouse_save.BackColor = System.Drawing.Color.Lavender;
+            this.btn_add_warehouse_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add_warehouse_save.Location = new System.Drawing.Point(250, 374);
+            this.btn_add_warehouse_save.Name = "btn_add_warehouse_save";
+            this.btn_add_warehouse_save.Size = new System.Drawing.Size(169, 44);
+            this.btn_add_warehouse_save.TabIndex = 12;
+            this.btn_add_warehouse_save.Text = "Save";
+            this.btn_add_warehouse_save.UseVisualStyleBackColor = false;
+            this.btn_add_warehouse_save.Click += new System.EventHandler(this.btn_add_warehouse_save_Click);
+            // 
+            // btn_add_warehouse_cancel
+            // 
+            this.btn_add_warehouse_cancel.BackColor = System.Drawing.Color.MistyRose;
+            this.btn_add_warehouse_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_add_warehouse_cancel.Location = new System.Drawing.Point(496, 374);
+            this.btn_add_warehouse_cancel.Name = "btn_add_warehouse_cancel";
+            this.btn_add_warehouse_cancel.Size = new System.Drawing.Size(169, 44);
+            this.btn_add_warehouse_cancel.TabIndex = 13;
+            this.btn_add_warehouse_cancel.Text = "Cancel";
+            this.btn_add_warehouse_cancel.UseVisualStyleBackColor = false;
+            this.btn_add_warehouse_cancel.Click += new System.EventHandler(this.btn_add_warehouse_cancel_Click);
+            // 
+            // txb_wh_id
+            // 
+            this.txb_wh_id.Location = new System.Drawing.Point(213, 68);
+            this.txb_wh_id.Name = "txb_wh_id";
+            this.txb_wh_id.Size = new System.Drawing.Size(175, 26);
+            this.txb_wh_id.TabIndex = 14;
+            // 
+            // txb_wh_name
+            // 
+            this.txb_wh_name.Location = new System.Drawing.Point(213, 116);
+            this.txb_wh_name.Name = "txb_wh_name";
+            this.txb_wh_name.Size = new System.Drawing.Size(175, 26);
+            this.txb_wh_name.TabIndex = 15;
+            // 
+            // txb_city
+            // 
+            this.txb_city.Location = new System.Drawing.Point(213, 266);
+            this.txb_city.Name = "txb_city";
+            this.txb_city.Size = new System.Drawing.Size(175, 26);
+            this.txb_city.TabIndex = 17;
+            // 
+            // txb_street
+            // 
+            this.txb_street.Location = new System.Drawing.Point(213, 215);
+            this.txb_street.Name = "txb_street";
+            this.txb_street.Size = new System.Drawing.Size(175, 26);
+            this.txb_street.TabIndex = 16;
+            // 
+            // txb_zip
+            // 
+            this.txb_zip.Location = new System.Drawing.Point(213, 312);
+            this.txb_zip.Name = "txb_zip";
+            this.txb_zip.Size = new System.Drawing.Size(175, 26);
+            this.txb_zip.TabIndex = 18;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.textBox6.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox6.Location = new System.Drawing.Point(592, 65);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(175, 26);
+            this.textBox6.TabIndex = 19;
+            this.textBox6.Text = "sm_iig";
+            // 
+            // txb_tax
+            // 
+            this.txb_tax.Location = new System.Drawing.Point(592, 116);
+            this.txb_tax.Name = "txb_tax";
+            this.txb_tax.Size = new System.Drawing.Size(175, 26);
+            this.txb_tax.TabIndex = 20;
+            // 
+            // txb_country
+            // 
+            this.txb_country.Location = new System.Drawing.Point(592, 215);
+            this.txb_country.Name = "txb_country";
+            this.txb_country.Size = new System.Drawing.Size(175, 26);
+            this.txb_country.TabIndex = 21;
+            // 
+            // txb_mobile
+            // 
+            this.txb_mobile.Location = new System.Drawing.Point(592, 266);
+            this.txb_mobile.Name = "txb_mobile";
+            this.txb_mobile.Size = new System.Drawing.Size(175, 26);
+            this.txb_mobile.TabIndex = 22;
+            // 
+            // cbx_state
+            // 
+            this.cbx_state.FormattingEnabled = true;
+            this.cbx_state.Items.AddRange(new object[] {
+            "Alaska",
+            "Alabama",
+            "Arizona",
+            "Arkansas",
+            "California",
+            "Colorado",
+            "Connecticut",
+            "Delaware",
+            "Florida",
+            "Georgia",
+            "Idaho",
+            "Illinois",
+            "Indiana",
+            "Kentucky",
+            "Louisiana",
+            "Michigan",
+            "Nebraska",
+            "Nevada",
+            "Oklahoma",
+            "Oregon",
+            "Rhode Island",
+            ""});
+            this.cbx_state.Location = new System.Drawing.Point(592, 165);
+            this.cbx_state.Name = "cbx_state";
+            this.cbx_state.Size = new System.Drawing.Size(175, 28);
+            this.cbx_state.TabIndex = 23;
+            // 
+            // cbx_market
+            // 
+            this.cbx_market.FormattingEnabled = true;
+            this.cbx_market.Location = new System.Drawing.Point(213, 162);
+            this.cbx_market.Name = "cbx_market";
+            this.cbx_market.Size = new System.Drawing.Size(175, 28);
+            this.cbx_market.TabIndex = 24;
+            // 
+            // rbtn_active
+            // 
+            this.rbtn_active.AutoSize = true;
+            this.rbtn_active.Location = new System.Drawing.Point(592, 312);
+            this.rbtn_active.Name = "rbtn_active";
+            this.rbtn_active.Size = new System.Drawing.Size(77, 24);
+            this.rbtn_active.TabIndex = 25;
+            this.rbtn_active.TabStop = true;
+            this.rbtn_active.Text = "Active";
+            this.rbtn_active.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_inactive
+            // 
+            this.rbtn_inactive.AutoSize = true;
+            this.rbtn_inactive.Location = new System.Drawing.Point(686, 312);
+            this.rbtn_inactive.Name = "rbtn_inactive";
+            this.rbtn_inactive.Size = new System.Drawing.Size(89, 24);
+            this.rbtn_inactive.TabIndex = 26;
+            this.rbtn_inactive.TabStop = true;
+            this.rbtn_inactive.Text = "Inactive";
+            this.rbtn_inactive.UseVisualStyleBackColor = true;
+            // 
+            // add_warehouse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 445);
+            this.Controls.Add(this.rbtn_inactive);
+            this.Controls.Add(this.rbtn_active);
+            this.Controls.Add(this.cbx_market);
+            this.Controls.Add(this.cbx_state);
+            this.Controls.Add(this.txb_mobile);
+            this.Controls.Add(this.txb_country);
+            this.Controls.Add(this.txb_tax);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.txb_zip);
+            this.Controls.Add(this.txb_city);
+            this.Controls.Add(this.txb_street);
+            this.Controls.Add(this.txb_wh_name);
+            this.Controls.Add(this.txb_wh_id);
+            this.Controls.Add(this.btn_add_warehouse_cancel);
+            this.Controls.Add(this.btn_add_warehouse_save);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "add_warehouse";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Warehouse";
+            this.Load += new System.EventHandler(this.add_warehouse_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button btn_add_warehouse_save;
+        private System.Windows.Forms.Button btn_add_warehouse_cancel;
+        private System.Windows.Forms.TextBox txb_wh_id;
+        private System.Windows.Forms.TextBox txb_wh_name;
+        private System.Windows.Forms.TextBox txb_city;
+        private System.Windows.Forms.TextBox txb_street;
+        private System.Windows.Forms.TextBox txb_zip;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox txb_tax;
+        private System.Windows.Forms.TextBox txb_country;
+        private System.Windows.Forms.TextBox txb_mobile;
+        private System.Windows.Forms.ComboBox cbx_state;
+        private System.Windows.Forms.ComboBox cbx_market;
+        private System.Windows.Forms.RadioButton rbtn_active;
+        private System.Windows.Forms.RadioButton rbtn_inactive;
+    }
+}
