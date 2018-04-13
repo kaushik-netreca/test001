6f64ca9217560110754e2c02f6c2dce0e6d70c03
kaushik ponugoti <kaushik@netreca.com>
2018-04-13 12:23:54 -0700

    update warehouse

--- test001/home_page.cs
+++ test001/home_page.cs
@@ -17,10 +17,61 @@ namespace test001
         {
             InitializeComponent();
             lb_user_name.Text = user_name;
+            tb_ctrl_1.DrawItem += new DrawItemEventHandler(tabControl1_DrawItem);
+            tb_ctrl_1.SelectedTab = tabPage1;
+        }
+        private void tabControl1_DrawItem(Object sender, System.Windows.Forms.DrawItemEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush _textBrush;
+
+            // Get the item from the collection.
+            TabPage _tabPage = tb_ctrl_1.TabPages[e.Index];
+
+            // Get the real bounds for the tab rectangle.
+            Rectangle _tabBounds = tb_ctrl_1.GetTabRect(e.Index);
+
+            if (e.State == DrawItemState.Selected)
+            {
+
+                // Draw a different background color, and don't paint a focus rectangle.
+                _textBrush = new SolidBrush(Color.Red);
+                g.FillRectangle(Brushes.Gray, e.Bounds);
+            }
+            else
+            {
+                _textBrush = new System.Drawing.SolidBrush(e.ForeColor);
+                e.DrawBackground();
+            }
+
+            // Use our own font.
+            Font _tabFont = new Font("Segoe Script", (float)12.0, FontStyle.Bold);
+            //Segoe Script, 12pt, style = Bold
+
+            // Draw string. Center the text.
+            StringFormat _stringFlags = new StringFormat();
+            _stringFlags.Alignment = StringAlignment.Center;
+            _stringFlags.LineAlignment = StringAlignment.Center;
+            g.DrawString(_tabPage.Text, _tabFont, _textBrush, _tabBounds, new StringFormat(_stringFlags));
         }
 
         private void Home_page_Load(object sender, EventArgs e)
         {
+            cbx_market.Text = "LA";
+            cbx_market.Items.Add("LA");
+            cbx_market.Items.Add("OC");
+            cbx_market.Items.Add("SB");
+            cbx_market.Items.Add("SD");
+            cbx_market.Items.Add("NC");
+            tabPage1.Text = "Warehouse";
+            tabPage2.Text = "Store";
+            tabPage3.Text = "Employee";
+            tabPage4.Text = "Purchase Order";
+            tabPage5.Text = "Sales Order";
+            tabPage6.Text = "Inventory";
+            tabPage7.Text = "Invoice";
+            tabPage8.Text = "Configuration";
+            tabPage9.Text = "Settings";
         }
 
         private void Btn_signout_Click(object sender, EventArgs e)
@@ -35,27 +86,10 @@ namespace test001
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_add_warehouse_Click(object sender, EventArgs e)
         {
-            this.Close();
-            Form1 lg1 = new Form1();
-            lg1.Show();
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            this.Close();
-            Form2 lg2 = new Form2();
-            lg2.Show();
-
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            this.Close();
-            Form3 lg3 = new Form3();
-            lg3.Show();
-
+            add_warehouse aw = new add_warehouse();
+            aw.Show();
         }
     }
 }
