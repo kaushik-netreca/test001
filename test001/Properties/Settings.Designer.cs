daa6ae8dc839f42cce9161e40b7d90dbddf55589
kaushik ponugoti <kaushik@netreca.com>
2018-04-13 12:23:22 -0700

    update warehouse

--- test001/Properties/Settings.Designer.cs
+++ test001/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace test001.Properties
-{
-
-
+namespace test001.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("server=wrpmaster-cluster.cluster-cumxizp3csel.us-west-1.rds.amazonaws.com;user id" +
+            "=posmasterus;password=Posmasterus-12;database=WRP_WHOLESALE")]
+        public string WRP_WHOLESALEConnectionString {
+            get {
+                return ((string)(this["WRP_WHOLESALEConnectionString"]));
             }
         }
     }
